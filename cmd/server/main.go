ea391dce436cacc9d9ee64b07bfa02a6b71be815
SinnerK9 <suairino6@gmail.com>
2026-05-16 16:58:09 +0800

    basic server with graceful shutdown

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -2,46 +2,66 @@ package main
 
 import (
 	"context"
-	"log"
+	"errors"
+	"log/slog"
 	"net/http"
 	"os"
 	"os/signal"
 	"syscall"
 	"time"
-
-	"github.com/gin-gonic/gin"
-	// 这里替换成你的模块名，比如 github.com/yourname/my-iot-server/internal/handler
-	"github.com/SinnerK9/my-iot-server/internal/handler"
 )
 
 func main() {
-	r := gin.Default()
+	// 结构化日志:new方法用JSON格式日志处理器创建一个Logger对象
+	// 将这个Logger对象设置为全局默认日志器
+	// 这个日志处理器将JSON输出到标准输出stdout，并且只输出info级别以上的日志
+	slog.SetDefault(slog.New(slog.NewJSONHandler(os.Stdout, &slog.HandlerOptions{
+		Level: slog.LevelInfo,
+	})))
+	//监听本机8080
+	addr := ":8080"
 
-	// 注册路由，把具体的逻辑甩给 handler 层去处理
-	// 这就是分层！main.go 变得极其干净！
-	r.GET("/v1/health", handler.Ping)
+	//ServeMux是Go的路由管理器，其作用是根据请求路径找到对应的处理函数
+	mux := http.NewServeMux()
+	//前一个字符串指定路径
+	//后面的匿名函数即为处理函数，w用于给客户端返回数据，r是客户端发来的请求
+	mux.HandleFunc("v1/health", func(w http.ResponseWriter, r *http.Request) {
+		w.Header().Set("Content-Type", "application/json") //返回格式是JSON
+		w.Write([]byte(`{"status":"ok"}`))
+	})
 
 	srv := &http.Server{
-		Addr:    ":8080",
-		Handler: r,
+		Addr:              addr,
+		Handler:           mux,              //交给上面创造的路由mux处理请求
+		ReadHeaderTimeout: 10 * time.Second, //十秒超时
 	}
 
+	//开启一个goroutine，防止阻塞的ListenAndServe影响后续关闭代码的执行
 	go func() {
-		log.Println("启动服务器...")
-		if err := srv.ListenAndServe(); err != nil && err != http.ErrServerClosed {
-			log.Fatalf("listen: %s\n", err)
+		slog.Info("Server starting", "addr", addr)
+		//如果不是正常关闭的错误，打印日志并且退出
+		if err := srv.ListenAndServe(); err != nil && !errors.Is(err, http.ErrServerClosed) {
+			slog.Error("listen failed", "err", err)
+			os.Exit(1)
 		}
 	}()
-
-	quit := make(chan os.Signal, 1)
-	signal.Notify(quit, syscall.SIGINT, syscall.SIGTERM)
-	<-quit
-	log.Println("收到关闭信号，准备优雅退出...")
-
-	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+	//用signal.NotifyContext来监听ctrl + c和SIGTERM这两个关闭信号
+	//注意必须传入一个context.Background作为根通知器
+	//返回一个context用于等待信号，一个stop函数用于触发停止
+	ctx, stop := signal.NotifyContext(
+		context.Background(),
+		os.Interrupt,
+		syscall.SIGTERM,
+	)
+	defer stop()
+	<-ctx.Done()                  //在这里阻塞住，等待关闭信号的到来
+	slog.Info("shutting down...") //得到信号了，从这里开始关闭流程
+	//一个新的停止通知器shutdownCtx，超过五秒就不等了，cancel和stop同理
+	shutdownCtx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
 	defer cancel()
-	if err := srv.Shutdown(ctx); err != nil {
-		log.Fatal("强制关闭:", err)
+	//执行关闭服务器，并将是否出错存到err里，有错误则打印日志
+	if err := srv.Shutdown(shutdownCtx); err != nil {
+		slog.Error("Shutdown error", "err", err)
 	}
-	log.Println("优雅退出完成")
+	slog.Info("bye")
 }
